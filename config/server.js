07aee51371d61bc96dab1a125caa97729cf8e92c
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-03 11:51:18 -0600

    Se corrige la direccion del enrutemiento en user

--- config/server.js
+++ config/server.js
@@ -12,7 +12,7 @@ class Server{
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
-        this.userPath = 'api/user'
+        this.userPath = '/api/user'
 
         this.conectarDB();
         this.middlewares();
