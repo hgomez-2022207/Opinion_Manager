f51fa87b90ef0ccb51455c5d4e405496fb441a79
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-03 11:49:58 -0600

    e crea el crearUsuario en route

--- src/user/user.route.js
+++ src/user/user.route.js
@@ -1,12 +1,23 @@
 import { Router } from "express";
 import {check} from "express-validator"
 
-//import { validarCampos } from "../middlewares/validar-campos.js";
+import { validarCampos } from "../middlewares/validar-campos.js";
 //import { validarJWT } from "../middlewares/validar-jwt.js";
-import {  } from "./user.controller.js";
-//import { existeEmail } from "../helpers/db-validators.js"
+import {  CreateUser } from "./user.controller.js";
+import { existeEmail } from "../helpers/db-validators.js"
 
 const router = Router();
+
+router.post('/',
+    [
+        check('name','invalid user').not().isEmpty(),
+        check('email','invalid email').not().isEmpty().isEmail(),
+        check('email').custom(existeEmail),
+        check('password','password is not valid').isLength({min:6}),
+        validarCampos
+    ], CreateUser
+
+);
 
 
 export default router;
