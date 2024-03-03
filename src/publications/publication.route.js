dd6931e70ae09e12babb8cf6dbfc2d6e7e4b13e3
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-03 17:06:08 -0600

    Se crea post en route para publicacion

---
+++ src/publications/publication.route.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import { CratePublication } from "./publication.controller.js";
+import { validarCampos } from "../middlewares/validar-campos.js";
+
+const router = Router();
+
+router.post(
+    '/',
+    [
+        check('qualification', 'The qualification is requiredio').not().isEmpty(),
+        check('category', 'The category is requiredio').not().isEmpty(),
+        check('text', 'The text is requiredio').not().isEmpty(),
+        validarCampos,
+    ],
+    CratePublication
+);
+
+export default router;
