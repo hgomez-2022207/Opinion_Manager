8605f3bca9a479d5883dee39634b6e77577639f4
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-03 03:12:55 -0600

    Se crea el route

---
+++ src/user/user.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import {check} from "express-validator"
+
+//import { validarCampos } from "../middlewares/validar-campos.js";
+//import { validarJWT } from "../middlewares/validar-jwt.js";
+import {  } from "./user.controller.js";
+//import { existeEmail } from "../helpers/db-validators.js"
+
+const router = Router();
+
+
+export default router;
