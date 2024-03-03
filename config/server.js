b2005abd72cf2d554d6aa504d475f3aa50e2d311
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-03 03:10:46 -0600

    Se a;ade la informacion de server.js

---
+++ config/server.js
@@ -0,0 +1,46 @@
+'use strict'
+
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { dbConnection } from './mongo.js';
+
+import userRoutes from '../src/user/user.route.js';
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.userPath = 'api/user'
+
+        this.conectarDB();
+        this.middlewares();
+        this.routes();
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.urlencoded({extended: false}));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(helmet());
+        this.app.use(morgan('dev'));
+    }
+
+    routes(){
+        this.app.use(this.userPath,userRoutes)
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Servidor Ejecutandose y escuchando el puerto',this.port)
+        });
+    }
+    
+}
+
+export default Server;
