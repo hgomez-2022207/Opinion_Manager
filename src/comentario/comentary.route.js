0ae3f5313f506f2f200167e24fb828d39237db96
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-03 19:13:16 -0600

    Se crea el agregar Comentario en el route

--- src/comentario/comentary.route.js
+++ src/comentario/comentary.route.js
@@ -1,16 +1,15 @@
-const { Router } = require('express');
-const { check } = require ('express-validator');
- 
- 
-const {comentaryPost} = require('./comentary.controller.js');
-const { validarCampos} = require ('../middlewares/validar-campos');
- 
+import { Router } from "express";
+import {check} from "express-validator"
+
+import { validarCampos } from "../middlewares/validar-campos.js";
+import {  comentaryPost } from "./comentary.controller.js";
+  
 const router = Router();
  
 router.post(
     "/",
     [
-        check("user", "User is not valido").not().isEmpty(),
+        check("email", "User is not valido").not().isEmpty(),
          check("comentary","Comentary is not valid").not().isEmpty(),
          validarCampos,
  
