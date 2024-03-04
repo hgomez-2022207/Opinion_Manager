23e19885af83f6e91173be3c3808cdb7f3282731
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-03 20:57:12 -0600

    Se agrega validar token

--- src/comentario/comentary.route.js
+++ src/comentario/comentary.route.js
@@ -3,12 +3,14 @@ import {check} from "express-validator"
 
 import { validarCampos } from "../middlewares/validar-campos.js";
 import {  comentaryPost } from "./comentary.controller.js";
+import { validarJWT } from "../middlewares/validar-jwt.js";
   
 const router = Router();
  
 router.post(
     "/",
     [
+        validarJWT,
         check("email", "User is not valido").not().isEmpty(),
          check("comentary","Comentary is not valid").not().isEmpty(),
          validarCampos,
