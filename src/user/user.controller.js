d0e64cb507a912bd27b8d58f18119dbdafba91ab
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-03 20:59:52 -0600

    Se crea update en usuario

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -18,3 +18,52 @@ export const CreateUser = async (req, res) => {
         user
     });
 }
+
+export const PutUser = async (req, res = response) =>{
+    const { name, email, password} = req.body;
+
+    const us = await User.findOne({email});
+
+    console.log('hiiiiiiiiiiiiii',us);
+
+    if (!us) {
+        console.log('hi');
+        return res.status(400).json({
+            msg: "El usuario no existe en la base de datos, porfavor, verifique qeu el correo sea el correcto"
+        });
+    }
+
+    //await User.findByIdAndUpdate( name, email, password)
+    us.name=name;
+    us.password=password;
+    const user = await User.findByIdAndUpdate(us.id, us);
+
+    res.status(200).json({
+        msg: "this user is update",
+        user
+    });
+}
+
+/*export const PutUser = async (req, res) => {
+    const { name } = req.params; // La cualificación de la publicación que deseas actualizar
+    const { email, password } = req.body;
+
+    try {
+        // Busca la publicación por su cualificación y actualízala con los nuevos datos
+        const updateUser = await User.findOneAndUpdate({ name }, { email, password }, { new: true });
+
+        if (!updatedPublication) {
+            return res.status(404).json({ message: 'Publicación no encontrada' });
+        }
+
+        res.json(updateUser); // Devuelve la publicación actualizada
+
+        res.status(200).json({
+            msg: 'Business Actualizado',
+            busines,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al actualizar la publicación' });
+    }
+};*/
--- src/user/user.route.js
+++ src/user/user.route.js
@@ -2,9 +2,9 @@ import { Router } from "express";
 import {check} from "express-validator"
 
 import { validarCampos } from "../middlewares/validar-campos.js";
-//import { validarJWT } from "../middlewares/validar-jwt.js";
-import {  CreateUser } from "./user.controller.js";
+import {  CreateUser, PutUser } from "./user.controller.js";
 import { existeEmail } from "../helpers/db-validators.js"
+import { validarJWT } from "../middlewares/validar-jwt.js";
 
 const router = Router();
 
@@ -16,6 +16,17 @@ router.post('/',
         check('password','password is not valid').isLength({min:6}),
         validarCampos
     ], CreateUser
+
+);
+
+router.put('/',
+    [
+        validarJWT,
+        check('name','invalid user').not().isEmpty(),
+        check('email','invalid email').not().isEmpty().isEmail(),
+        check('password','password is not valid').isLength({min:6}),
+        validarCampos
+    ], PutUser
 
 );
 
