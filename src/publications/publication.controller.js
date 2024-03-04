6d4dac7247382a8bd7bef5bbb6714f61cf011377
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-03 20:59:31 -0600

    Se crea update en controller

--- src/publications/publication.controller.js
+++ src/publications/publication.controller.js
@@ -14,3 +14,27 @@ export const CratePublication = async (req, res) => {
         publication
     });
 }
+
+export const PutPublication = async (req, res = response) =>{
+    const { qualification, category, text} = req.body;
+
+    const p = await Public.findOne({qualification});
+
+
+    if (!p) {
+        console.log('hi');
+        return res.status(400).json({
+            msg: "Esta publicacion no existe en la base de datos, porfavor, verifique qeu el titulo sea el correcto y no intente cambiar el titulo"
+        });
+    }
+    p.qualification=qualification;
+    p.category=category;
+    p.text=text
+
+    const pub = await Public.findByIdAndUpdate(p.id, p);
+
+    res.status(200).json({
+        msg: "this publication is update",
+        pub
+    });
+}
