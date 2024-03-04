e91f3974aad4bb7d7eba63753279df308969fd4f
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-03-03 19:12:57 -0600

    Se crea el agregar Comentario

--- src/comentario/comentary.controller.js
+++ src/comentario/comentary.controller.js
@@ -2,12 +2,14 @@ import User from '../user/user.model.js';
 import Public from '../publications/publication.model.js'
  
 export const comentaryPost = async(req, res) =>{
-    const { email, comentary } = req.body;
+    const { email, comentary, qualification } = req.body;
     try {
         console.log('hi');
         const user = await User.findOne({email});
-        const pub = await Public.findOne({comentary})
- 
+        const pub = await Public.findOne({qualification})
+        console.log('pub',pub)
+
+        console.log(user);
         if (!user) {
             console.log('hi2');
             return res.status(400).json({
@@ -15,7 +17,12 @@ export const comentaryPost = async(req, res) =>{
             });
         }
 
-        pub.email.push(user);
+        if (!pub) {
+            console.log('no se ha enctrado la publicacion');
+            
+        }
+
+        pub.user.push(user.email);
         pub.comentary.push(comentary);
 
         await pub.save();
@@ -26,14 +33,9 @@ export const comentaryPost = async(req, res) =>{
         });
  
     } catch (e) {
-        console.log('hi4');
+        console.log('hi4', e);
         res.status(500).json({
-            msg:  `error al unirse a un grupo`
+            msg:  `error when entering comment`
         });
     }
-}
- 
-module.exports = {
-    joinPost,
-    joinDelete
 }
